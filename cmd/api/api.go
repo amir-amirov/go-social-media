115b46f4ac0720b6cdf86bff1ceb752ac982e9fe
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-06-08 20:19:24 +0500

    Added chi router

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 type application struct {
 	config config
@@ -18,13 +25,35 @@ func newApplication(addr string) *application {
 	}
 }
 
-func (app *application) run() error {
-	mux := http.NewServeMux()
+func (app *application) mount() *chi.Mux {
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	// return mux
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
+}
+
+func (app *application) run(mux *chi.Mux) error {
 
 	srv := &http.Server{
-		Addr:    app.config.addr,
-		Handler: mux,
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30, // time it takes for server to send response back to client
+		ReadTimeout:  time.Second * 10, // time it takes for server from receiving the first packet to the last packet after tcp connection is formed
+		IdleTimeout:  time.Minute,      // time it takes for tcp connection remains alive after sending response back to the client
 	}
+
+	log.Printf("Launching server on port%v", app.config.addr)
 
 	return srv.ListenAndServe()
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func main() {
 	app := newApplication(":8080")
+	mux := app.mount()
 
-	if err := app.run(); err != nil {
-		log.Fatalf("[ERROR] Unable to run application..")
+	if err := app.run(mux); err != nil {
+		log.Fatalf("[ERROR] Unable to launch server..")
 	}
 }
