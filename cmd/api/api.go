973003a37f608f78bc4ebb73ea7f152ce8816c1c
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-06-10 23:47:43 +0500

    Added storage, user repo, post repo

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,21 +5,31 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/amir-amirov/go-social-media/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
 	config config
+	store  store.Storage
 }
 
 type config struct {
 	addr string
+	db   dbConfig
 }
 
-func newApplication(cfg config) *application {
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+}
+
+func newApplication(cfg config, store store.Storage) *application {
 	return &application{
 		config: cfg,
+		store:  store,
 	}
 }
 
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/amir-amirov/go-social-media/internal/env"
+	"github.com/amir-amirov/go-social-media/internal/store"
 	"github.com/joho/godotenv"
 )
 
@@ -13,11 +14,19 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	store := store.NewPostgresStorage(nil)
+
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:password@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+		},
 	}
 
-	app := newApplication(cfg)
+	app := newApplication(cfg, store)
+
 	mux := app.mount()
 
 	if err := app.run(mux); err != nil {
